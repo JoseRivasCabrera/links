f98d97efe25c93a8d0721ae1424851587a95a50e
Rivas <rivas.dev.contacto@gmail.com>
2024-06-12 08:01:26 -0600

    Actualizar Precios

--- public/scripts/linkbio.js
+++ public/scripts/linkbio.js
@@ -94,7 +94,7 @@ cajaInfo.addEventListener("click", function eventos(event) {
          </div>
       
          <div class="caja-precios">
-           <p class="precio-servicio"><span class="simbolo-dolar">$</span>6,45</p>
+           <p class="precio-servicio"><span class="simbolo-dolar">$</span>6,65</p>
            <p class="precio-descuento"><span>$</span>12,90</p>
          </div>
       
@@ -120,7 +120,7 @@ cajaInfo.addEventListener("click", function eventos(event) {
          </div>
       
          <div class="caja-precios">
-           <p class="precio-servicio"><span class="simbolo-dolar">$</span>10,45</p>
+           <p class="precio-servicio"><span class="simbolo-dolar">$</span>10,65</p>
            <p class="precio-descuento"><span>$</span>20,90</p>
          </div>
       
