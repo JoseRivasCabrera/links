374ed2ff19e8d4186333d15036acefadf3a75732
rivas.dev.contacto <rivas.dev.contacto@gmail.com>
2024-07-19 07:37:49 +0000

    se desarrolla la seccion de practicas y se hace responsive

---
+++ public/scripts/practicas.js
@@ -0,0 +1,33 @@
+const practicas = `<section class="caja-proyecto-practica">
+
+<div class="proyecto-practica">
+
+  <h2 class="nombre-practica">Creación de una página de perfil personal</h2>
+
+<div class="caja-contenido-practica">
+    <div class="caja-tecnologias-practica">
+        <h2 class="titulo-tecnologias">Tecnologías</h2>
+        <img src="public/img/iconos/vscode-icons--file-type-html.svg" alt="HTML Icon" class="icono-practica">
+        <img src="public/img/iconos/vscode-icons--file-type-css.svg" alt="CSS Icon" class="icono-practica">
+    </div>
+
+    <div class="dificultad">
+        <h3 class="titulo-dificultad">Dificultad</h3>
+        <img src="public/img/iconos/fire-color.svg" alt="Fire Icon">
+        <img src="public/img/iconos/fire-color.svg" alt="Fire Icon">
+        <img src="public/img/iconos/fire-color.svg" alt="Fire Icon">
+        <img src="public/img/iconos/noto--fire.svg" alt="Fire Icon">
+        <img src="public/img/iconos/noto--fire.svg" alt="Fire Icon">
+    </div>
+</div>
+
+<div class="caja-descripcion"> 
+    <h3 class="descripcion-practica">Crear una página web que muestre información básica sobre ustedes mismos. Esto incluirá su nombre, una foto de perfil, información de contacto y una breve descripción.</h3>
+    <div class="caja-btn-practica">
+        <a href="#" class="btn-ver-practica">Ver Práctica</a>
+    </div>
+</div>
+
+</div>
+
+</section>`
